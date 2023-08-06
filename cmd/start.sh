76aa4a062d49bd49660cf9cadcf0453c92233f05
ThaisGLeite <thaisdecarvalho@proton.me>
2023-08-06 08:52:13 -0300

    container name update

--- cmd/start.sh
+++ cmd/start.sh
@@ -8,13 +8,13 @@ docker-compose -f ../docker/docker-compose.yml up --build -d vault
 
 # Wait for the Vault server to start
 echo "Waiting for Vault server to start..."
-while ! docker logs docker-vault-1 2>&1 | grep -q 'Vault server started! Log data will stream in below:'
+while ! docker logs docker_vault_1 2>&1 | grep -q 'Vault server started! Log data will stream in below:'
 do
   sleep 1
 done
 
 # Initialize the Vault server and store the keys in keys.txt
-docker exec docker-vault-1 vault operator init > keys.txt
+docker exec docker_vault_1 vault operator init > keys.txt
 
 # Extract unseal keys and root token
 KEY1=$(grep 'Unseal Key 1:' keys.txt | awk '{print $NF}')
@@ -26,17 +26,17 @@ ROOT_TOKEN=$(grep 'Initial Root Token:' keys.txt | awk '{print $NF}')
 rm keys.txt
 
 # Unseal the Vault
-docker exec docker-vault-1 vault operator unseal $KEY1
-docker exec docker-vault-1 vault operator unseal $KEY2
-docker exec docker-vault-1 vault operator unseal $KEY3
+docker exec docker_vault_1 vault operator unseal $KEY1
+docker exec docker_vault_1 vault operator unseal $KEY2
+docker exec docker_vault_1 vault operator unseal $KEY3
 
 # Print the root token and Export the root token as VAULT_TOKEN
 echo "Vault Root Token: $ROOT_TOKEN"
 export VAULT_TOKEN=$ROOT_TOKEN
 
 # Store MongoDB credentials in Vault 
-docker exec -e VAULT_TOKEN=$ROOT_TOKEN docker-vault-1 vault secrets enable -path=secret kv
-docker exec -e VAULT_TOKEN=$ROOT_TOKEN docker-vault-1 vault kv put secret/data/mongodb data='{"username":"thaisdev","password":"DevEnv123"}'
+docker exec -e VAULT_TOKEN=$ROOT_TOKEN docker_vault_1 vault secrets enable -path=secret kv
+docker exec -e VAULT_TOKEN=$ROOT_TOKEN docker_vault_1 vault kv put secret/data/mongodb data='{"username":"thaisdev","password":"DevEnv123"}'
 
 # Start the MongoDB Docker container
 docker-compose -f ../docker/docker-compose.yml up -d mongodb
